9ee24acfbaa08247798829481d6874dc747aca1c
Maxime Wetz <maxime-wetz@hotmail.com>
2018-11-13 11:41:42 +0100

    Changing the roots for players

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [] do
-    resources :reservation
-    resources :player
+  resources :users, only: [:show] do
+    resources :reservations
+    resources :players
     resources :reviews
   end
 
