02e76cdcd687b1f8b59fad60d8425cb4d79ae7eb
Julian Stas <julian.stas@gmail.com>
2018-11-13 12:39:22 +0100

    added reservation table

--- db/migrate/20181112152352_create_reservations.rb
+++ db/migrate/20181112152352_create_reservations.rb
@@ -1,7 +1,10 @@
 class CreateReservations < ActiveRecord::Migration[5.2]
   def change
     create_table :reservations do |t|
-
+      t.date :booked_from
+      t.date :booked_to
+      t.references :user, foreign_key: true
+      t.references :field, foreign_key: true
       t.timestamps
     end
   end
