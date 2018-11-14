bc504fc2541163c06e7d6c1a4ec01bf716949580
Maxime Wetz <maxime-wetz@hotmail.com>
2018-11-14 10:25:46 +0100

    pundit for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,22 +1,26 @@
 class ReservationsController < ApplicationController
   def index
-    @reservations = Reservation.all
+    @reservations = policy_scope(Reservation)
   end
 
   def show
     @reservation = Reservation.find(params[:id])
+    authorize @reservation
   end
 
   def new
     @reservation = Reservation.new
+    authorize @reservation
   end
 
   def edit
     @reservation = Reservation.find(params[:id])
+    authorize @reservation
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
+    authorize @reservation
     if @reservation.save
       redirect_to root_path
     else
@@ -26,6 +30,7 @@ class ReservationsController < ApplicationController
 
   def update
     @reservation = Reservation.update(reservation_params)
+    authorize @reservation
     if @reservation.update(reservation_params)
       redirect_to profile_reservations_path(reservations), notice: 'Reservation was successfully updated.'
     else
@@ -42,6 +47,6 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:timetable, :field_id, :user_id)
+    params.require(:reservation).permit(:booking_day, :booking_hour_start, :booking_hour_end, :field_id, :user_id)
   end
 end
