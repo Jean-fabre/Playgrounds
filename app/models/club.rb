3208787a2206319d8176e9cc4ea89b4b205b58e0
Maxime Wetz <maxime-wetz@hotmail.com>
2018-11-15 15:51:30 +0100

    navbar ready

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,6 +4,7 @@ class Club < ApplicationRecord
   has_many :fields
   has_many :events, through: :fields
   validates :user_id, uniqueness: true
+  validates :user_id, uniqueness: true
   validates :name, presence: true
   validates :address, presence: true
 
