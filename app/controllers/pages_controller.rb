8fcf71241740d0b3f51ddf68cc7c1396853de48e
Maxime Wetz <maxime-wetz@hotmail.com>
2018-11-15 10:46:39 +0100

    new player profile after sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @player = Player.new
+    @player = current_user.player.nil? ? Player.new : current_user.player
     @player.user = current_user
   end
 end
