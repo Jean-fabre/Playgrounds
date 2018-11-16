8fb14a345a932c3661be3170aea3b1e13500597a
Julian Stas <julian.stas@gmail.com>
2018-11-16 13:27:23 +0100

    added dashboard functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   before_action :set_field
 
   def home
+    # --------------DASHBOARD BEGIN----do not touch-------------
     # All events for a club - dashboard
     @upcoming_events = []
     @fields.each do |f|
@@ -33,20 +34,19 @@ class PagesController < ApplicationController
       end
     end
     # end revenue for this week
-
+  # ------------------DASHBOARD END-------------------------
   end
 
   private
-
+  # User checks to prevent crashes
   def set_field
     if user_signed_in?
       @player = current_user.player.nil? ? Player.new : current_user.player
       @player.user = current_user
     end
-    if current_user.club.name != nil
+    if current_user.club.name
       @club = current_user.club
       @fields = @club.fields
     end
   end
-
 end
