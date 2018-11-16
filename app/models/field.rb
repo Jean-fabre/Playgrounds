e06b45a144d4796778cd238dee8b99355e3dbb6c
christophe moorkens <christophe.moorkens@gmail.com>
2018-11-16 14:55:37 +0100

    searchfield

--- app/models/field.rb
+++ app/models/field.rb
@@ -8,4 +8,14 @@ class Field < ApplicationRecord
   validates :field_type, presence: true, inclusion: { in: ["Tennis", "Squash", "Padel"] }
   validates :price, presence: true
   mount_uploader :photo, PhotoUploader
+
+  include PgSearch
+  pg_search_scope :global_search,
+    against: [:field_type],
+    associated_against: {
+      club: [:address]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
